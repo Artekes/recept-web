11b1cc6ea1ce022c0297066b1850fc311f0b57bc
Artekes <artsym.exe@gmail.com>
2026-03-31 17:18:07 +0200

    Created variable localStorag in script.js and function to add recepts
    I can load later recipes from local storage

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const main = document.getElementById(`main`);
+let localStorage = [];
+
+function addRecept(imageURL, titleStr, descriptionStr, raitingInt, timeInt) {
+	localStorage.push([imageURL, titleStr, descriptionStr, raitingInt, timeInt]);
+}
