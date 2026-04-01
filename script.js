28634070034eb3857ff88d19a843e4b9a3017c99
Artekes <artsym.exe@gmail.com>
2026-04-01 10:35:23 +0200

    Added the targetURL parametr to fun and obj localStorage
    So button knows, where to redirect user

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let localStorage = [{
 		imageURL: "https://picsum.photos/300/300",
 		titleStr: "Test Recept",
 		descriptionStr: "Just test recept",
+		targetURL: "",
 		raitingInt: 8,
 		timeInt: 5
 	}];
 
-function addRecept(imageURL, titleStr, descriptionStr, raitingInt, timeInt) { // function to add recepts to localStorage
+function addRecept(imageURL, titleStr, descriptionStr, targetURL, raitingInt, timeInt) { // function to add recepts to localStorage
 	let recipeInfo = {
 		imageURL: imageURL,
 		titleStr: titleStr,
 		descriptionStr: descriptionStr,
+		targetURL: targetURL,
 		raitingInt: raitingInt,
 		timeInt: timeInt
 	};
@@ -27,7 +29,7 @@ function loadRecepts() {
 						   <h2 class="title">${element.titleStr}</h2>
 						   <p class="description">${element.descriptionStr}</p>
 						   <div> 
-						   	   <button onclick="addRecept('https://picsum.photos/300/300', 'Test Recept', 'Just test recept', 8, 50);">Дивитись рецепт</button>
+						   	   <a href="${element.targetURL}<button onclick="addRecept('https://picsum.photos/300/300', 'Test Recept', 'Just test recept', 8, 50);">Дивитись рецепт</button>
 						   	   <span class="raiting"><i class="fa-regular fa-star"></i>${element.raitingInt}/10</span>
 						   	   <span class="time"><i class="fa-regular fa-clock"></i>${element.timeInt}хв</span>
 						   </div>
